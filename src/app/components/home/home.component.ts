60161e2054b8200b027ffa95ac1b94797211778b
seishi1 <major_seishi@hotmail.com>
2019-01-08 07:22:51 -0300

    fin del home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,14 +7,7 @@ import { ejemplo } from  '../../ejemplo';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  ejemplo1: ejemplo = {
-    id: 1,
-    name: "ejemploHome"
-  };
   inicio = "Ofrecemos servicios de limpieza de calidad, ajustado a tus necesidades, ya sea una oficina u otro tipo de inmueble, ponemos la mejor gente a tu disposición, contáctanos y trabajemos juntos!";
-  imgPrincipal ="";
-  imgBajada1 ="";
-  imgBajada2 ="";
   constructor() { }
 
   ngOnInit() {
